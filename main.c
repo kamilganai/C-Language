30a5253a3c196157a1f170f1afe8be12f804ae9a
Kamil Ganai <kamilganai786@gmail.com>
2026-09-13 22:25:58 +0530

    first

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main(){
-    int a = 2;
-    int b = 5;
-    float c = 5.5;
-    char d = 'hi';
-printf("THE VALUE OF a is %d \n" , a);
-printf("THE VALUE OF b is %f \n" , b);
-printf("THE VALUE OF c is %c \n" , c);
-return 0;
+int a , b;
+printf("ENTER THE VALUE OF a: ");
+scanf("%d",&a);
+printf("ENTER THE VALUE OF b: ");  
+scanf("%d",&b);
+printf("THE SUM OF a AND b IS: %d",a+b);
 
+
+
+
+
+    return 0;
 }
-pooooooooooooooooooooooooooooo

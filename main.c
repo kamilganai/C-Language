2cac03a11ae83bf01dc3ff031285e3ab4446a0fd
Kamil Ganai <kamilganai786@gmail.com>
2026-09-13 18:13:16 +0530

    update

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main(){
     int a = 2;
-    float b = 5.5;
-    char c = 'hi';
+    int b = 5;
+    float c = 5.5;
+    char d = 'hi';
 printf("THE VALUE OF a is %d \n" , a);
 printf("THE VALUE OF b is %f \n" , b);
 printf("THE VALUE OF c is %c \n" , c);
